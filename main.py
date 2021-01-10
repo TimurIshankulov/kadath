4142965379c90b93540ca5efbfc63d6f75ed0f89
Timur Ishankulov <tim290592@gmail.com>
2021-01-10 17:22:21 +0300

    Add API functions

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, make_response, session, flash, redirect, url_for
+from flask import Flask, render_template, request, make_response, session, flash, redirect, url_for, Response
 from flask_bootstrap import Bootstrap
 import datetime
 import os
+import json
 from shutil import copyfile
 import config
 from forms import LoginForm
@@ -14,6 +15,18 @@ app = Flask(__name__)
 app.secret_key = config.flask_secret_key
 app.permanent_session_lifetime = datetime.timedelta(days=365)
 Bootstrap(app)
+
+
+def to_json(data):
+    return json.dumps(data) + "\n"
+
+
+def resp(code, data):
+    return Response(
+        status=code,
+        mimetype="application/json",
+        response=to_json(data)
+    )
 
 
 @app.route('/')
@@ -37,6 +50,25 @@ def login():
 @app.route('/kadath', methods=['GET', 'POST'])
 def kadath():
     return render_template(kadath_template)
+
+
+@app.route('/kadath/notes', methods=['GET'])
+def get_notes():
+    with open('notes/notes.json', 'r') as f:
+        data = json.load(f)
+    return resp(200, data)
+
+
+@app.route('/kadath/note/<int:note_id>', methods=['GET'])
+def get_note(note_id):
+    with open('notes/notes.json', 'r') as f:
+        data = json.load(f)
+    for record in data:
+        if record['id'] == note_id:
+            note = record
+            break
+    return note
+
 
 if __name__ == "__main__":
     app.debug = True
