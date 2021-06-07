ff7a4d055022034f77e1605dda533fcf63c6f463
Timur Ishankulov <tim290592@gmail.com>
2021-06-07 15:27:41 +0300

    Add sign up form

--- forms.py
+++ forms.py
@@ -3,7 +3,17 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Label
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class SignUpForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired()])
+    firstname = StringField('Firstname', validators=[DataRequired()])
+    lastname = StringField('Lastname', validators=[DataRequired()])
+    registration_secret = StringField('Registration Secret', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
